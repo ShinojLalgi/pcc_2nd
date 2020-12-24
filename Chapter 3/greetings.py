ed6dfe1e9d4b9bb80c11b2cfd5a853431c4f0685
Shinoj Lalgi <69868383+ShinojLalgi@users.noreply.github.com>
2020-12-24 22:42:02 +0530

    Create greetings.py

---
+++ Chapter 3/greetings.py
@@ -0,0 +1,6 @@
+names = ['john', 'anushka', 'mathew', 'mary', 'joseph']
+print(f"Hi {names[0].title()}!")
+print(f"Hi {names[1].title()}!")
+print(f"Hi {names[2].title()}!")
+print(f"Hi {names[3].title()}!")
+print(f"Hi {names[4].title()}!")
