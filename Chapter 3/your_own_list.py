d4c59831acc3e462e3b42c1a0edf64a6c5fdf22e
Shinoj Lalgi <69868383+ShinojLalgi@users.noreply.github.com>
2020-12-24 22:57:34 +0530

    Create your_own_list.py

---
+++ Chapter 3/your_own_list.py
@@ -0,0 +1,5 @@
+transportation = ['car', 'bus', 'train', 'boat']
+print(f"I would like to drive a {transportation[0]}.")
+print(f"I hate travelling in a crowded {transportation[1]}.")
+print(f"I couldn't get a ticket for the last {transportation[2]}.")
+print(f"I wish I could build a wooden {transportation[3]}.")
