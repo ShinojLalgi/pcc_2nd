f7e25b317728cf2f9c544b5a34c13b412c656a13
Shinoj Lalgi <69868383+ShinojLalgi@users.noreply.github.com>
2020-12-24 23:28:45 +0530

    Create pizza.py

---
+++ Chapter 4/pizza.py
@@ -0,0 +1,6 @@
+favorite_pizzas = ['classic', 'smoked', 'pineapple']
+
+for pizza in favorite_pizzas:
+    print(f"I love {pizza.title()} pizza.")
+
+print("I wish I could order all thses pizzas right now.")
