369252fcbed14f072b204fcf8c7c1dc0d1e480c2
Shinoj Lalgi <69868383+ShinojLalgi@users.noreply.github.com>
2020-12-25 12:03:12 +0530

    Add files via upload

---
+++ Chapter 5/favorite_fruits.py
@@ -0,0 +1,8 @@
+favorite_fruits = ['mango', 'kiwi', 'apple']
+
+if 'apple' in favorite_fruits:
+    print("I love apple.")
+elif 'mango' in favorite_fruits:
+    print('I love mango.')
+else:
+    print(" love kiwi.")
---
+++ Chapter 5/stages_of_life.py
@@ -0,0 +1,14 @@
+age = 2
+
+if age < 2:
+    print("You are a baby.")
+elif 2 <= age < 4:
+    print("You are a toddler.")
+elif 4 <= age < 13:
+    print("You are a kid.")
+elif 13 <= age < 20:
+    print("You are a teenager.")
+elif 20 <= age < 65:
+    print("You are a adult.")
+else:
+    print("You are a elder.")
