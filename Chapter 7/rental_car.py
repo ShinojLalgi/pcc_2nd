830a28862401b635bb76b9735b09c53659321c62
Shinoj Lalgi <69868383+ShinojLalgi@users.noreply.github.com>
2020-12-25 20:42:17 +0530

    Add files via upload

---
+++ Chapter 7/rental_car.py
@@ -0,0 +1,2 @@
+car = input("Which card do you want? ")
+print(f"Let's see if we can find you a {car.title()}.")
---
+++ Chapter 7/restuarant_setting.py
@@ -0,0 +1,6 @@
+people = input("How many people are in your group? ")
+
+if int(people) > 8:
+    print("You guys will have to wait for some time to get a table.")
+else:
+    print("Your table is ready now.")
