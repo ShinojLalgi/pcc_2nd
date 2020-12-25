54df3dcb4ffec8f1dc35d174496dc4ffded75bc6
Shinoj Lalgi <69868383+ShinojLalgi@users.noreply.github.com>
2020-12-25 19:46:58 +0530

    Add files via upload

---
+++ Chapter 6/gloassary.py
@@ -0,0 +1,12 @@
+gloassary = {
+    'print()' : 'Prints the values to a stream, or to sys.stdout by default.',
+    'min()' : 'With a single iterable argument, return its smallest item.',
+    'max()' : 'With a single iterable argument, return its biggest item.',
+    'sorted()' : 'Return a new list containing all items from the iterable in ascending order.',
+    'list()' : 'If no argument is given, the constructor creates a new empty list.'
+}
+
+for key, value in gloassary.items():
+    print(key + ':')
+    print(value)
+    print()
