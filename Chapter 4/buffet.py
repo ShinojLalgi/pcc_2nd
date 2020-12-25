cdba50d88a1b63049b44d9e45c67a612fd5a111d
Shinoj Lalgi <69868383+ShinojLalgi@users.noreply.github.com>
2020-12-25 11:45:10 +0530

    Add files via upload

---
+++ Chapter 4/buffet.py
@@ -0,0 +1,12 @@
+buffet_food = {'pizza', 'burger', 'pasta', 'cake', 'pie'}
+
+print("The buffet offers the following food:")
+for food in buffet_food:
+    print(food)
+
+
+buffet_food = {'pizza', 'burger', 'pasta', 'bhel', 'soup'}
+
+print("\nThe buffet offers the following food:")
+for food in buffet_food:
+    print(food)
