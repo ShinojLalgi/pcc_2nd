cefef238e90919334e8bd6bb8e4e5aa0bf945cb7
Shinoj Lalgi <69868383+ShinojLalgi@users.noreply.github.com>
2020-12-25 11:53:28 +0530

    Create conditional_tests.py

---
+++ Chapter 5/conditional_tests.py
@@ -0,0 +1,14 @@
+word = 'sun'
+
+print(word == 'sun')
+print(word != 'sky')
+print( 3 > 1)
+print(2 < 1)
+print( 2 >= 2)
+print( 2 <= 2)
+
+words = ['sun', 'moon', 'sky']
+
+print('moon' in words)
+print('sun' not in words)
+print('sun' in words and 'sky' in words)
