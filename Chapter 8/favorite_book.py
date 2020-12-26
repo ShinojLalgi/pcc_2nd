fab0ab78162e0f1c68cc7166faf15af98363c8c7
Shinoj Lalgi <69868383+ShinojLalgi@users.noreply.github.com>
2020-12-26 12:06:13 +0530

    Add files via upload

---
+++ Chapter 8/favorite_book.py
@@ -0,0 +1,5 @@
+def favorite_book(book_title):
+    """ Prints the title of your favorite book """
+    print(f"Your favorite books is '{book_title.title()}'.")
+
+favorite_book('alice in wonderland')
---
+++ Chapter 8/message.py
@@ -0,0 +1,5 @@
+def display_message():
+    """ Displays a message """
+    print("We are learning about functions in this chapter.")
+
+display_message()
